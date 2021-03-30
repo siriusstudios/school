c59b724a99b3859f2a0ac1d752c50f0cfe9cc6b7
siriusstudios <60844722+siriusstudios@users.noreply.github.com>
2021-03-30 20:33:05 +0300

    new school hw

---
+++ forcycle.pas
@@ -0,0 +1,34 @@
+program bruh;
+
+var i, a, b, amebi,counter : integer;
+//var x : real;
+//begin 
+//For i:=10 to 5 do 
+//begin 
+//writeln('ok', i);
+//end;
+//
+//For i:=15 downto 0 do
+//begin
+//writeln('ok', i);
+//end;
+//
+
+
+
+amebi := 1;
+//x := 1;
+
+For i:= 1 to 8 do
+begin
+//x := power(2, i-1)*2;
+amebi:= amebi * 2; 
+
+write(' Амёб ', amebi);
+//write(' Амёб ', x); // solution 2
+writeln(' Час: ', (i*3));
+end;
+
+
+end.
+
